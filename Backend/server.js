37ce559bdd1f080161df441350cecc2681067452
9397karan <144720736+9397karan@users.noreply.github.com>
2025-02-20 23:55:10 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 const server = http.createServer(app); // Create HTTP server
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://lms-dup.onrender.com",
     credentials: true,
   },
 });
