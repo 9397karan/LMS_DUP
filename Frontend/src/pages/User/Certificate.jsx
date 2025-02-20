d9d4f081ba5751394b68557a98867756754b376d
9397karan <144720736+9397karan@users.noreply.github.com>
2025-02-20 23:40:31 +0530

    Update Certificate.jsx

--- Frontend/src/pages/User/Certificate.jsx
+++ Frontend/src/pages/User/Certificate.jsx
@@ -17,7 +17,7 @@ const Certificate = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.post("http://localhost:5000/api/certificates", { userId, courseId });
+                const response = await axios.post("https://backend-dup.onrender.com/api/certificates", { userId, courseId });
                 if (response.status === 200) {
                     setUser(response.data.user);
                     setCourse(response.data.course);
